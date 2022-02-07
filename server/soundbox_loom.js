91c523c7756fd88aa56fe44ddc32d057c6712148
Leow92 <lr.baesen@gmail.com>
2022-02-07 16:12:25 +0100

    sites

--- server/soundbox_loom.js
+++ server/soundbox_loom.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./sources/dedicatedbrand');
+const loom = require('./sites/loom');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+async function sandbox (eshop = 'https://www.loom.fr/collections/tous-les-vetements') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    const products = await loom.scrape(eshop);
 
     console.log(products);
     console.log('done');
