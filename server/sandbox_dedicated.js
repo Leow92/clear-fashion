36cd766519dae36f2102b06029055750585a46c6
Leow92 <lr.baesen@gmail.com>
2022-02-07 17:57:57 +0100

    site dedicated

--- server/sandbox_dedicated.js
+++ server/sandbox_dedicated.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console, no-process-exit */
-const dedicatedbrand = require('./sources/dedicatedbrand');
+const dedicatedbrand = require('./sources/dedicated');
 
 async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/all-men') {
   try {
