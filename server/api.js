5aad4299ca0b8b4f6f45371e6323238ba20bf186
Leow92 <lr.baesen@gmail.com>
2022-03-21 16:20:46 +0100

    update products search

--- server/api.js
+++ server/api.js
@@ -1,7 +1,6 @@
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
-//const {calculateLimitAndOffset, paginate} = require('paginate-info');
 const {MongoClient} = require('mongodb');
 const MONGODB_URI = 'mongodb+srv://Leow92:bE3bLbq3mJSjT!J3@cluster0.wnwww.mongodb.net/?retryWrites=true&w=majority';
 const MONGODB_DB_NAME = 'Cluster0';
@@ -18,6 +17,10 @@ app.use(helmet());
 
 app.options('*', cors());
 
+app.listen(PORT);
+
+console.log(`📡 Running on port ${PORT}`);
+
 app.get('/', (request, response) => {
   response.send({'ack': true});
 });
@@ -28,27 +31,28 @@ app.get('/products', (request, response) => {
   var res=allProducts().then(res => response.send(res));
 });
 
+app.get('/products/search', (request, response) => {
+  var limit=request.query.limit;
+  var brand=request.query.brand;
+  var price=request.query.price;
+  response.send(request.query);
+  limit = parseInt(limit);
+  price = parseInt(price);
+  //var page=request.query.page;
+  //var brand = splt[]
+  console.log(limit);
+  console.log(brand);
+  console.log(price);
+  //console.log(page)
+  //var res=searchProductsBrands(brand).then(res => response.send(res));
+  var res=searchProducts(limit,brand,price).then(res => response.send(res));
+});
+
 app.get('/products/:id', (request, response) => {
   var url=request.url; //ok
   var splitURL=url.split("/").pop();
   var res=productsById(splitURL).then(res => response.send(res));
 });
-
-app.get('/products/search', (request, response) => {
-  let brand=request.query.brand;
-  let price=request.query.price;
-  let limit=request.query.limit;
-  let page=request.query.page;
-  console.log(brand)
-  console.log(price)
-  console.log(limit)
-  console.log(page)
-  var res=searchProducts(brand,price,limit,page).then(res => response.send(res));
-});
-
-app.listen(PORT);
-
-console.log(`📡 Running on port ${PORT}`);
 
 const productsById = async(id)=>{
   await connect();
@@ -66,37 +70,30 @@ const allProducts = async()=>{
   return prod
 }
 
-const searchProducts = async(brand,price,limitation,page) => {
-  let b=[]
-  if (brand===undefined) {b=["adresse","dedicated","montlimar"];}
-  else {b.push(brand);}
-
-  let v=0
-  if (price===undefined) {v=10000000}
-  else {v.push(price);}
-
-  let l=0
-  if (limitation===undefined) {l=12;}
-  else {l=parseInt(limitation);}
-
-  let p=0
-  if (page===undefined) {p=1;}
-  else {p=parseInt(page);}
-  
+const searchProducts = async(limitation,brand,price) => {
   await connect();
   collection = db.collection('products');
+  console.log("Hello");
+  const count = await collection.find({"brand":brand},{"price":{$lt:price}}).count();
+  console.log(count);
 
-  const count = await collection.find({brand:{$in:b}},{price:{$lt:v}}).count();
-  console.log(count)
-
-  const {limit,offset} = calculateLimitAndOffset(page, limitation);
+  //const {limit,offset} = calculateLimitAndOffset(page, limitation);
   
-  const products = await collection.find({brand:{$in:b}},{price:{$lt:v}}).skip(offset).limit(limit).toArray();
-
-  const meta = paginate (p, count, products, limitation);
-  return({products,meta})
+  const products = await collection.find({"brand":brand},{"price":{$lt:price}}).limit(limitation).toArray();
+  console.log(products);
+  //const meta = paginate (p, count, products, limitation);
+  return(products)
 }
 
+const searchProductsBrands = async(brand) => {
+  await connect();
+  collection = db.collection('products');
+  const count = await collection.find({"brand":brand}).count();
+  console.log(count);
+  const products = await collection.find({"brand":brand}).toArray();
+  console.log(products);
+  return(products)
+}
 
 const connect = async () => {
   try{
